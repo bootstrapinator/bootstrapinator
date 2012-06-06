aab63494067b5e3ba18025f78067228f29bd1104
Brendan Saunders <bluesaunders@gmail.com>
2012-06-05 20:44:00 -0400

    prelim copy and basic structure

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+$(function(){
+  $("#hero-comparison").beforeAfter({
+    animateIntro: true,
+    introDelay: 250,
+    imagePath: '/assets/js/',
+    cursor: 'col-resize',
+    dividerColor: '#f15738',
+    enableKeyboard: true,
+    keypressAmount: 50
+  });
+});
