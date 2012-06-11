9f8d1708cf8df895f75b647e552a432f9bd249f2
Aaron White <aaron@callmeaaron.com>
2012-06-11 09:42:04 -0400

    cleanup to put on ftp server

--- README.md
+++
@@ -1,8 +0,0 @@
-# Bootstrapinator Coming Soon.
-
-Custom themes for Twitter's bootstrap framework.
-
-### Co-Founders:
-
-* [@internetAaron](https://twitter.com/internetaaron "Follow Aaron on Twitter!")
-* [@blue642](https://twitter.com/blue642 "Follow Brendan on Twitter!")
---
+++ _site/assets/js/app.js
@@ -0,0 +1,13 @@
+$(function(){
+  $("#hero-comparison").beforeAfter({
+    animateIntro: true,
+    introDelay: 250,
+    imagePath: '/assets/js/',
+    cursor: 'col-resize',
+    dividerColor: '#f15738',
+    enableKeyboard: true,
+    keypressAmount: 50,
+    beforeLinkText: "Bootstrap",
+    afterLinkText: "BoostrapINATOR"
+  });
+});
